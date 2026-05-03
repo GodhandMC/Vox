bcba53985fe4526715e2770509324c9d853cda3c
adelemphii <demilitioowner@gmail.com>
2026-05-03 12:24:35 -0500

    base plugin

---
+++ src/main/java/mc/godhand/vox/Vox.java
@@ -0,0 +1,17 @@
+package mc.godhand.vox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Vox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
